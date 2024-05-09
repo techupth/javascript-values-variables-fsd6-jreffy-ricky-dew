abe960ebf3e01b436fca7c07a35eb91111e198ed
Jeffiros <krittiwat.c@gmail.com>
2024-05-09 13:49:24 +0700

    feat:complete exercises

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,31 @@
 //Exercise #1: Variable Creation
-//Start coding here
+/* ให้ Declare ตัว Variable ที่สามารถ Reassign Value ได้ ทั้งหมด 3 Variable ตามข้อกำหนดดังต่อไปนี้
+
+1.1 Variable ที่เก็บ Value ที่เป็นชื่อของคนซึ่งมีชื่อคือ "Karter"
+1.2. Variable ที่เก็บ Value ที่เป็นนามสกุลของคนซึ่งมีนามสกุลคือ "Anderson"
+1.3. Variable ที่เก็บ Value ที่เป็นที่อยู่ซึ่งมีที่อยู่เป็น "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai"
+ให้ Declare ตัว Variable ที่ไม่สามารถ Reassign Value ได้ ทั้งหมด 3 Variable ตามข้อกำหนดดังต่อไปนี้
+
+1.4 Variable ที่เก็บ Value ชื่อร้านอาหารซึ่งมีชื่อเป็น "Chef's Table by Chef James"
+1.5 Variable ที่เก็บ Value ของราคาอาหารสุทธิในบิลนั้นซึ่งมีราคาเป็น 15000
+1.6 Variable ที่เก็บ Value ของจำนวนร้านอาหารที่เปิดอยู่ตอนนี้ทั้งหมดซึ่งมี 1500 ร้าน */
+
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 Bangkruay-Sainoi Bang Kruay Bang Kruai";
+
+const restaurantName = "Chef's Table by Chef James";
+const totalAmount = 15000;
+const totalOpeningShop = 1500;
+
 
 //Exercise #2: Variable Reassignment
-//Start coding here
+/* ให้นำตัว Variable ที่สร้างจาก Exercise ที่ 1 มา Reassign ตัว Value ใหม่ เข้าไปตามข้อกำหนดดังต่อไปนี้
+ให้ Reassign ตัว Value ของ Variable ที่อยู่ (ในข้อที่ 1.3) เป็น "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”
+ให้ Reassign ตัว Value ของ Variable ราคาอาหารสุทธิ (ในข้อที่ 1.5) เป็น 5000
+ให้ Reassign ตัว Value ของ Variable จำนวนร้านอาหารที่เปิดอยู่ตอนนี้ทั้งหมด (ในข้อที่ 1.6) เป็น 2500 */
+
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641";
+totalAmount = 5000;
+totalOpeningShop = 2500;
+
--- ex-3.js
+++ ex-3.js
@@ -1,6 +1,9 @@
 // Exercise #3: Celsius to Fahrenheit
+/* celsius เป็น Variable แบบ let ที่เก็บ Value ของค่าอุณหภูมิในหน่วย Celsius เป็น 30
+ให้เขียน Expression ในการแปลงอุณหภูมิในหน่วย Celsius เป็นหน่วย Fahrenheit
+โดยสูตรการหาอุณหภูมิในหน่วยฟาเรนไฮต์คือ (celsius x 1.8) + 32
+จากนั้นเก็บผลลัพธ์เข้าไปในตัว Variable ที่มีชื่อว่า fahrenheit */
+
 let celsius = 30;
-
-// Start coding here;
-
-let fahrenheit;
+let fahrenheit = (celsius * 1.8) + 32;
+console.log(fahrenheit);
