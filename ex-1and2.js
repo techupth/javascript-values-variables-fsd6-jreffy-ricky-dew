802d9d60dfae0fa103434cdcc4f2b24df6b28679
Wascharapong Phummon <rickyharvey.w@gmail.com>
2024-05-09 14:13:23 +0700

    feat: Ex-1-4

--- ex-1and2.js
+++ ex-1and2.js
@@ -1,5 +1,18 @@
 //Exercise #1: Variable Creation
 //Start coding here
+let firstName = "Karter";
+let lastName = "Anderson";
+let address = "54/24 Gp 5 BangKruay-Sainoi Bang Kruay Bang Kruai";
+const restaurantName = "Chef's Table by Chef James";
+const billAmount = 15000;
+const totalRestaurants = 1500;
 
 //Exercise #2: Variable Reassignment
 //Start coding here
+address = "(970) 878-53292400 County Rd #4Meeker, Colorado(CO), 81641”;
+netBillPrice = 5000;
+totalRestaurantsOpen = 2500;
+
+
+
+
